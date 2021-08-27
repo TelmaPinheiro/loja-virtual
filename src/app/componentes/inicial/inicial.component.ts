6e95a26ad82df2e6a3af22abef819d6d745e2829
TelmaPinheiro <telmapy@yahoo.com.br>
2021-08-27 08:01:19 -0300

    Descrição do produto e carrinho de compras.

--- src/app/componentes/inicial/inicial.component.ts
+++ src/app/componentes/inicial/inicial.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Produto } from 'src/app/models/produto.model';
+import { CarrinhoService } from 'src/app/servicos/carrinho.service';
 import { ProdutoService } from 'src/app/servicos/produto.service';
-
-
 
 @Component({
   selector: 'app-inicial',
@@ -10,14 +10,22 @@ import { ProdutoService } from 'src/app/servicos/produto.service';
   styleUrls: ['./inicial.component.css']
 })
 export class InicialComponent implements OnInit {
-  
+
   produtos: Produto[] = [];
 
-  constructor(private produtoService: ProdutoService) { }
+  constructor(
+    private produtoService: ProdutoService,
+    private router: Router,
+    private carrinhoService: CarrinhoService
+  ) { }
 
   ngOnInit(): void {
     this.produtos = this.produtoService.listarTodos();
   }
 
-}
+  adicionarCarrinho(produto: Produto) {
+    this.carrinhoService.adicionar(produto);
+    this.router.navigate(['/carrinho']);
+  }
 
+}
